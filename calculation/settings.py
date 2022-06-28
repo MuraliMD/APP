06e9953ec8be69c53a6a87ed641a13fd684c72f1
Muralidharan <muralivijay2804@gmail.com>
2022-06-28 17:56:49 +0530

    commit

--- calculation/settings.py
+++ calculation/settings.py
@@ -133,4 +133,5 @@ STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-django_heroku.settings(locals())
+
+heroku config:set DISABLE_COLLECTSTATIC=1
