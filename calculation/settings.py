6e6906bb783018628d5436760adeee94ef6906fe
Muralidharan <muralivijay2804@gmail.com>
2022-06-28 18:05:18 +0530

    commit

--- calculation/settings.py
+++ calculation/settings.py
@@ -119,12 +119,10 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_ROOT = os.path.join(PROJECT_ROOT, 'staticfiles')
 STATIC_URL = '/static/'
+STATIC_ROOT =(BASE_DIR, '/static/')
 
-STATICFILES_DIRS = (os.path.join(PROJECT_ROOT, 'static')),
 
-STATICFILES_STORAGE = 'whitenoise.django.GzipManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
